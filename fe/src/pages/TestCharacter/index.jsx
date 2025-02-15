4cb9f0abd1682b9673f417390d4d4f46d52ebf0d
Phuonglp03 <tvp141003@gmail.com>
2025-02-15 15:35:58 +0700

    add picture to Layout

--- fe/src/pages/TestCharacter/index.jsx
+++ fe/src/pages/TestCharacter/index.jsx
@@ -1,15 +1,21 @@
 import CustomNavbar from '@/components/Navbar';
-import { Button, Col, Row, Typography } from 'antd';
+import { Button, Col, Row, Typography, Image } from 'antd';
 import React from 'react';
 
 const { Title, Text } = Typography;
 
 const TestCharacter = () => {
+
     return (
         <div style={{ backgroundColor: '#fff', minHeight: '100vh' }}>
             <CustomNavbar />
-            <div style={{ height: '80vh', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#f0f0f0', marginBottom: '40px' }}>
-                <Text strong style={{ fontSize: '24px', color: '#666' }}>Chưa có ảnh</Text>
+            <div style={{
+                height: '90vh', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#f0f0f0', marginBottom: '40px', backgroundImage: "url('./Thêm tiêu đề.png')",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat"
+            }}>
+                {/* <Image src='./Thêm tiêu đề.png ' preview={false} style={{ width: "100%", height: "100%", objectFit: "cover" }}  /> */}
             </div>
 
 
@@ -41,40 +47,46 @@ const TestCharacter = () => {
                         </Button>
                     </Col>
                     <Col span={6}>
-                        <div style={{ height: '50vh', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#f0f0f0' }}>
-                            <Text strong style={{ fontSize: '20px', color: '#666' }}>Chưa có ảnh</Text>
+                        <div style={{ height: '50vh', display: 'flex', alignItems: 'center', justifyContent: 'center', margin: 'auto', backgroundImage: `url("./Tied in Passion, Freed in Love Intense Play, Sweet Ending (6).png")`, backgroundSize: "contain", backgroundPosition: "center", backgroundRepeat: "no-repeat" }}>
+                            
                         </div>
                     </Col>
                 </Row>
             </div>
 
             <div style={{ padding: '0 15vh 15vh 15vh' }}>
-                    <Title level={3} style={{ marginTop: '40px', fontWeight: 'bold', textTransform: 'uppercase', fontFamily: 'Inter, sans-serif' }}>
-                        CÁC KIỂU TÍNH CÁCH ENNEAGRAM
-                    </Title>
-                    <Row gutter={[100, 50]} style={{ marginTop: '40px' }} justify="center">
-                        {["Cầu Toàn", "Giúp Đỡ", "Thành Đạt", "Cá Tính", "Nghiên Cứu"].map((name, index) => (
-                            <Col key={index} xs={12} sm={8} md={6} lg={4} style={{ textAlign: 'center' }}>
-                                <div style={{ backgroundColor: '#FA6400', color: 'white', borderRadius: '50%', width: '150px', height: '150px', display: 'flex', alignItems: 'center', justifyContent: 'center', margin: 'auto' }}>
+                <Title level={3} style={{ marginTop: '40px', fontWeight: 'bold', textTransform: 'uppercase', fontFamily: 'Inter, sans-serif' }}>
+                    CÁC KIỂU TÍNH CÁCH ENNEAGRAM
+                </Title>
+                <Row gutter={[100, 50]} style={{ marginTop: '40px' }} justify="center">
+                    {["Cầu Toàn", "Giúp Đỡ", "Thành Đạt", "Cá Tính", "Nghiên Cứu"].map((name, index) => (
+                        <Col key={index} xs={12} sm={8} md={6} lg={4} style={{ textAlign: 'center' }}>
+                            <div style={{ color: 'white', borderRadius: '50%', width: '150px', height: '150px', display: 'flex', alignItems: 'center', justifyContent: 'center', margin: 'auto', backgroundImage: `url("./biểu tượng 9 nhóm tính cách/${index + 1}.png")`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat" }}>
 
-                                </div>
-                                <Text strong style={{ color: '#333', fontSize: '16px', display: 'block' }}>Nhóm {index + 1}</Text>
-                                <Text style={{ fontSize: '14px', color: '#333', marginTop: '8px' }}>Người {name}</Text>
-                            </Col>
-                        ))}
-                    </Row>
-                    <Row gutter={[100, 50]} style={{ margin: '15vh' }} justify="center">
-                        {["Trung Thành", "Nhiệt Tình", "Thử Thách", "Hòa Bình"].map((name, index) => (
+                            </div>
+                            <Text strong style={{ color: '#333', fontSize: '16px', display: 'block' }}>Nhóm {index + 1}</Text>
+                            <Text style={{ fontSize: '14px', color: '#333', marginTop: '8px' }}>Người {name}</Text>
+                        </Col>
+                    ))}
+                </Row>
+                <Row gutter={[100, 50]} style={{ marginTop: '40px' }} justify="center">
+                    {["Trung Thành", "Nhiệt Tình", "Thử Thách", "Hòa Bình"].map((name, index) => {
+                        const actualIndex = index + 6;
+                        return (
                             <Col key={index} xs={12} sm={8} md={6} lg={4} style={{ textAlign: 'center' }}>
-                                <div style={{ backgroundColor: '#FA6400', color: 'white', borderRadius: '50%', width: '150px', height: '150px', display: 'flex', alignItems: 'center', justifyContent: 'center', margin: 'auto' }}>
-
+                                <div style={{ color: 'white', borderRadius: '50%', width: '150px', height: '150px', display: 'flex', alignItems: 'center', justifyContent: 'center', margin: 'auto', backgroundImage: `url("./biểu tượng 9 nhóm tính cách/${actualIndex}.png")`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat" }}>
                                 </div>
-                                <Text strong style={{ color: '#333', fontSize: '16px', display: 'block' }}>Nhóm {index + 1}</Text>
-                                <Text style={{ fontSize: '14px', color: '#333', marginTop: '8px' }}>Người {name}</Text>
+                                <Text strong style={{ color: '#333', fontSize: '16px', display: 'block' }}>
+                                    Nhóm {actualIndex}
+                                </Text>
+                                <Text style={{ fontSize: '14px', color: '#333', marginTop: '8px' }}>
+                                    Người {name}
+                                </Text>
                             </Col>
-                        ))}
-                    </Row>
-             </div>
+                        );
+                    })}
+                </Row>
+            </div>
         </div>
     );
 };
